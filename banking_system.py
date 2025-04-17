53a846e1a0fb121de3c718784a0a877e796c4736
DavidLuvsTheHub <davidkipkulei77@gmail.com>
2025-04-17 16:01:08 +0300

    Implemented banking system using OOP

--- banking_system.py
+++ banking_system.py
@@ -1,3 +1,10 @@
 def __init__(self, account_number, balance=0)
-    deposit = (balance + deposit)
-    withdrawal=
+    dep_amount(input(int())
+    balance = (balance + amount)
+    with_amount= (input(int())
+                  if with_amount< balance:
+                      print("insufficient balance")
+                  else:
+                        balance = (balance - with_amount)
+    get_balance(return balance
+                print balance)
